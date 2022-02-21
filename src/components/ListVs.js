5e132e1a015c7b52b0f5e014128b0883321cb331
amer <amermostaafa@gmail.com>
2022-02-21 06:57:01 +0200

    add currency convirter

--- src/components/ListVs.js
+++ src/components/ListVs.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
-import { ListGroup, Button, ListGroupItem } from "reactstrap";
+import { ListGroup, ListGroupItem } from "reactstrap";
 import "./ListVs.css";
 const ListVs = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+
   useEffect(() => {
-    fetch("https://curr-api-2.herokuapp.com/currency")
+    fetch("https://curr-api-2.herokuapp.com/currency/EUR")
       .then((res) => {
         return res.json();
       })
@@ -23,27 +24,26 @@ const ListVs = () => {
   }, []);
   if (error) {
     return <div>{error.message}</div>;
-  } else if (!items[0]) {
+  } else if (!isLoaded) {
     return <div>...loading</div>;
-  } else if (isLoaded) {
-    const myVsKey = () => Object.keys(items[0]?.vs);
+  } else if (items.vs) {
+    const myVsKey = () => Object.keys(items?.vs);
     return (
       <ListGroup flush className="col-lg-6" lang="en">
         <h1 className="left">
-          {items[0]?.name} <span className="vs">vs</span>
+          {items.name} <span className="vs">vs</span>
         </h1>
         {myVsKey().map((k, id) => {
           return (
             <ListGroupItem className="left" key={id}>
-              <span>{items[0].vs[k]}</span> {k}
+              <span>{items.vs[k]}</span> {k}
             </ListGroupItem>
           );
         })}
-        <Button color="success" outline>
-          load more
-        </Button>
       </ListGroup>
     );
+  }else {
+    return ( <div>no data</div>)
   }
 };
 
--- src/components/TransBox.js
+++ src/components/TransBox.js
@@ -1,42 +1,85 @@
+import { useState, useEffect } from "react";
 import { Form, FormGroup, Col, Input } from "reactstrap";
 import AdArea from "./AdArea";
 import "./TransBox.css";
 const TransBox = () => {
-  return (
-    <Form className="col-lg-6 form">
-      <FormGroup row>
-        <Col sm={10}>
-          <Input
-            bsSize="lg"
-            id="curreny-from"
-            name="curreny-from"
-            placeholder="0.0 EUR"
-            type="number"
-          />
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Col sm={10}>
-          <Input
-            id="currency-t"
-            name="currency-t"
-            placeholder="0.0"
-            type="number"
-          />
-        </Col>
-      </FormGroup>
-      <FormGroup className="select">
-        <Col sm={10}>
-          <Input id="exampleSelect" name="select" type="select">
-            <option>EUR</option>
-            <option>EG</option>
-            <option>SYP</option>
-            <option>usd</option>
-          </Input>
-        </Col>
-      </FormGroup>
-      <AdArea />
-    </Form>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://curr-api-2.herokuapp.com/currency/EUR")
+      .then((res) => {
+        return res.json();
+      })
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+  if (error) {
+    return <div>error : {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>....loading</div>;
+  } else if (items.vs) {
+    const vsKeys = Object.keys(items.vs);
+    const transHandler = () => {
+      const fromVal = document.getElementById("currency-from");
+      const toVal = document.getElementById("currency-t");
+      const toCurr = document.getElementById("s-to");
+      const price = items.vs[toCurr.value];
+      return toVal.value = price * fromVal.value;
+    };
+    return (
+      <Form className="col-lg-6 form">
+        <FormGroup row>
+          <Col sm={10}>
+            <Input
+              onChange={transHandler}
+              bsSize="lg"
+              id="currency-from"
+              name="curreny-from"
+              placeholder="0.0 EUR"
+              type="number"
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Col sm={10}>
+            <Input
+              id="currency-t"
+              name="currency-t"
+              placeholder="0.0"
+              type="number"
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup className="select">
+          <Col sm={10}>
+            <Input id="s-to" name="s-to" type="select">
+              {
+                vsKeys.map((item, id) => {
+                  return <option key={id}>{item}</option>;
+                })
+                // items.forEach((i) => {
+                //   console.log(i)
+                //   return <option>{i.vs.AED}</option>;
+                // })
+              }
+            </Input>
+          </Col>
+        </FormGroup>
+        <AdArea />
+      </Form>
+    );
+  } else {
+    return null;
+  }
 };
 export default TransBox;
